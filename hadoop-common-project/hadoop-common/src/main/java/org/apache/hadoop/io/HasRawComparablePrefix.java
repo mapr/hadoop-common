ad4b34743d64a93f9ce45dacc03e4441e5aac627
Kannan Rajah <krajah@maprtech.com>
2014-02-06 18:23:03 -0800

    Bug 13086: Improve Map task performance.
    Testing:
    Ran word count, tera sort.

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/HasRawComparablePrefix.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/HasRawComparablePrefix.java
@@ -1,5 +1,8 @@
 package org.apache.hadoop.io;
 
+import org.apache.hadoop.classification.MapRModified;
+
+@MapRModified(summary = "Improve map task performance - bug 5942")
 public interface HasRawComparablePrefix {
   public void getPrefix(byte[] dst, int off, int prefixLen);
 }
