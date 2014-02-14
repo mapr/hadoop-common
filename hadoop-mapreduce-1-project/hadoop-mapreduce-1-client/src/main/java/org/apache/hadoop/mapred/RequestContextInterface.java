bdf73acc059fbf407e5069a1f3743a95e63fe0f4
Kannan Rajah <krajah@maprtech.com>
2014-02-13 16:50:44 -0800

    Deleted MR1 code base.
    No longer needed. MR1 is under Mercurial.

--- hadoop-mapreduce-1-project/hadoop-mapreduce-1-client/src/main/java/org/apache/hadoop/mapred/RequestContextInterface.java
+++
@@ -1,8 +0,0 @@
-package org.apache.hadoop.mapred;
-
-public interface RequestContextInterface<ReturnType> {
-
-  public void sendResponse(ReturnType response);
-  
-  public void performRequest();
-}
