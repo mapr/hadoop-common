135cebd57bbf0eb9dfa7cef50310bc1d9db4baf7
Kannan Rajah <krajah@maprtech.com>
2014-03-20 15:45:03 -0700

    Bug 13652: Return Hadoop 1 version when MR1 is active.
    VersionInfo will look for mapreduce-version-info.properties file first.
    If this is absent, it will fall back to common-version-info.properties.
    In the case of Hadoop 2, since the mapreduce file is absent, it will
    have the default behavior. In the case of Hadoop 2, the maprecuce file
    is present and so it will print Hadoop 1 version.
    
    Testing:
    
     Hadoop 2
     $ /opt/mapr/hadoop/hadoop-2.3.0/bin/hadoop version
     Hadoop 2.3.0-SNAPSHOT
     Subversion git@github.com:mapr/private-hadoop-common.git -r
     f2e20bdd37ea992d34c0c1736009f0f2f888b6e7
     Compiled by krajah on 2014-03-20T22:41Z
     Compiled with protoc 2.5.0
     From source with checksum d9d769789863cf4828b94c83095dad0
     This command was run using
     /opt/mapr/hadoop/hadoop-2.3.0/share/hadoop/common/hadoop-common-2.3.0-SNAPSHOT.jar
    -----------------------------------------------------------------------------------
     Hadoop 1
     $ /opt/mapr/hadoop/hadoop-0.20.2/bin/hadoop version
    
     WARNING: org.apache.hadoop.metrics.jvm.EventCounter is deprecated.
     Please use org.apache.hadoop.log.metrics.EventCounter in all the
     log4j.properties files.
     Hadoop 1.0.3
     Subversion http://mapr.com -r f12174af7f29794040617c22ff25f7e5898579c2
     Compiled by krajah on Thu Mar 20 15:44:40 PDT 2014
     Compiled with protoc 2.5.0
     From source with checksum 7ec6ab661b0cf909d7148f4b3e02fdec
     This command was run using
     /opt/mapr/hadoop/hadoop-2.3.0/share/hadoop/common/hadoop-common-2.3.0-SNAPSHOT.jar

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/util/VersionInfo.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/util/VersionInfo.java
@@ -28,6 +28,8 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.classification.InterfaceAudience;
 import org.apache.hadoop.classification.InterfaceStability;
 
+import org.apache.hadoop.classification.MapRModified;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -48,7 +50,10 @@ public class VersionInfo {
     try {
       InputStream is = Thread.currentThread().getContextClassLoader()
         .getResourceAsStream(versionInfoFile);
-      info.load(is);
+
+      if (is != null) {
+        info.load(is);
+      }
     } catch (IOException ex) {
       LogFactory.getLog(getClass()).warn("Could not read '" + 
         versionInfoFile + "', " + ex.toString(), ex);
@@ -94,7 +99,21 @@ public class VersionInfo {
     return info.getProperty("protocVersion", "Unknown");
   }
 
-  private static VersionInfo COMMON_VERSION_INFO = new VersionInfo("common");
+  //private static VersionInfo COMMON_VERSION_INFO = new VersionInfo("common");
+  // Use mapreduce version instead of common version in order to distinguish
+  // between MR1 and MR2. This is because ecosystem components like Pig. Hive
+  // rely on it to load the approriate Hadoop Shim.
+  @MapRModified
+  private static VersionInfo COMMON_VERSION_INFO = new VersionInfo("mapreduce");
+  static {
+    // Fall back to common version if the mapreduce version file is not
+    // available. This can happen if an application did not include map reduce
+    // jar. Right now, we don't even have this file for MR2.
+    if (COMMON_VERSION_INFO.info.isEmpty()) {
+      COMMON_VERSION_INFO = new VersionInfo("common");
+    }
+  }
+
   /**
    * Get the Hadoop version.
    * @return the Hadoop version string, eg. "0.6.3-dev"
