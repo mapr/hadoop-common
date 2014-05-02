05e426b8bc45b209461d67e636872a8c48b09e44
Patrick Wong <pwong@maprtech.com>
2014-05-02 14:08:24 -0700

    Fix hadoop-common build for Windows

--- hadoop-common-project/hadoop-common/src/main/native/src/org/apache/hadoop/io/nativeio/NativeIO.c
+++ hadoop-common-project/hadoop-common/src/main/native/src/org/apache/hadoop/io/nativeio/NativeIO.c
@@ -598,6 +598,7 @@ JNIEXPORT void JNICALL Java_org_apache_hadoop_io_nativeio_NativeIO_00024POSIX_ch
 #endif
 }
 
+#ifdef UNIX
 /**
  * public static native void chown(String path, String user, String group) throws IOException;
  */
@@ -670,6 +671,7 @@ cleanup:
   if (newpath) (*env)->ReleaseStringUTFChars(env, j_newpath, newpath);
   return rc;
 }
+#endif
 
 /*
  * static native String getUserName(int uid);
