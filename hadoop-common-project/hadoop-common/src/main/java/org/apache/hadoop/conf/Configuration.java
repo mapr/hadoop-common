913498895d03d9f9e5632c7a240e08e1025f4546
Nikunj Bansal <nbansal@maprtech.com>
2014-05-02 14:08:22 -0700

    Fixed the issue with CoreDefaultProperties not getting loaded in Configuration.java after the 2.3.0 merge.

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/conf/Configuration.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/conf/Configuration.java
@@ -2567,6 +2567,7 @@ public class Configuration implements Iterable<Map.Entry<String,String>>,
         Properties props = null;
         if (!resourceStr.endsWith(".xml") && (props = getProperties(resourceStr)) != null) {
           overlay(properties, props);
+          return null;
         } else {
           URL url = getResource(resourceStr);
           doc = parse(builder, url);
