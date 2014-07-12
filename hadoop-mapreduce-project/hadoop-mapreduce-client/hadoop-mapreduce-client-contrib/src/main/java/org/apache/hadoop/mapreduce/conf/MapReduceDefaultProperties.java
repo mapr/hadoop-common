801587347ba4afa0f9ef23ebfb6aa7f6b59efe62
Kannan Rajah <krajah@maprtech.com>
2014-07-11 17:56:43 -0700

    MAPR-13859: Change default MR AM staging dir to RM volume.
    The volume and the staging dir are created as part of an auxiliary
    service called RMVolumeMnager during RM startup. This dir will be used
    for storing job history as well.
    
    Testing:
    Ran word count and verified the history files are created under RM
    volume.
    
    hadoop fs -ls -R /var/mapr/cluster/yarn/rm/staging/history
    drwxrwxrwt   - root root          1 2014-07-11 16:38 /var/mapr/cluster/yarn/rm/staging/history/done_intermediate
    drwxrwx---   - root root          3 2014-07-11 16:38 /var/mapr/cluster/yarn/rm/staging/history/done_intermediate/root
    -rwxrwx---   3 root root      33711 2014-07-11 16:38 /var/mapr/cluster/yarn/rm/staging/history/done_intermediate/root/job_1405121872516_0001-1405121895310-root-word+count-1405121922679-1-1-SUCCEEDED-default-1405121912780.jhist
    -rwxrwx---   3 root root        347 2014-07-11 16:38 /var/mapr/cluster/yarn/rm/staging/history/done_intermediate/root/job_1405121872516_0001.summary
    -rwxrwx---   3 root root      77669 2014-07-11 16:38 /var/mapr/cluster/yarn/rm/staging/history/done_intermediate/root/job_1405121872516_0001_conf.xml

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-contrib/src/main/java/org/apache/hadoop/mapreduce/conf/MapReduceDefaultProperties.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-contrib/src/main/java/org/apache/hadoop/mapreduce/conf/MapReduceDefaultProperties.java
@@ -18,7 +18,10 @@ public class MapReduceDefaultProperties extends Properties {
 
   static { // MapReduce framework related defaults
     props.put(MRConfig.FRAMEWORK_NAME, MRConfig.YARN_FRAMEWORK_NAME);
-    props.put(MRJobConfig.MR_AM_STAGING_DIR, "${fs.defaultFS}/tmp/staging");
+
+    // TODO Refer to YarnDefaultProperties.DEFAULT_RM_STAGING_DIR once this
+    // file is moved out to MapR code base.
+    props.put(MRJobConfig.MR_AM_STAGING_DIR, "${fs.defaultFS}/var/mapr/cluster/yarn/rm/staging");
   }
 
   static { // Direct shuffle configuration
