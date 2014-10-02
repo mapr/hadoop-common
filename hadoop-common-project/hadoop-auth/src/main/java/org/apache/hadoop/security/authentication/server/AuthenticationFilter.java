fb9fae82e0f0678cdbd79c32c81977b0841259c4
Swapnil Daingade <sdaingade@maprtech.com>
2014-10-02 01:30:04 -0700

    Removing DEFAULT_CONFIG_PREFIX_VALUE for now to allow ResourceManager to start after hadoop-common 2.5.1 merge. We may have to come back and visit this later. Will file a bug for this.

--- hadoop-common-project/hadoop-auth/src/main/java/org/apache/hadoop/security/authentication/server/AuthenticationFilter.java
+++ hadoop-common-project/hadoop-auth/src/main/java/org/apache/hadoop/security/authentication/server/AuthenticationFilter.java
@@ -136,11 +136,6 @@ public class AuthenticationFilter implements Filter {
   public static final String CONFIG_PREFIX = "config.prefix";
 
   /**
-   * Default configuration prefix value to use if its not specified.
-   */
-  public static final String DEFAULT_CONFIG_PREFIX_VALUE = "hadoop.http.authentication.";
-
-  /**
    * Constant for the property that specifies the authentication handler to use.
    */
   public static final String AUTH_TYPE = "type";
@@ -212,8 +207,7 @@ public class AuthenticationFilter implements Filter {
   @Override
   public void init(FilterConfig filterConfig) throws ServletException {
     configPrefix = filterConfig.getInitParameter(CONFIG_PREFIX);
-    configPrefix = (configPrefix != null) ? configPrefix + "."
-                                          : DEFAULT_CONFIG_PREFIX_VALUE;
+    configPrefix = (configPrefix != null) ? configPrefix + "." : "";
 
     config = getConfiguration(configPrefix, filterConfig);
     String authHandlerName = config.getProperty(AUTH_TYPE, null);
