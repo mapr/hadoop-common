4096cb1c6b942ac9b1a64fa0cd42197d43b0158d
Wangda Tan <wangda@apache.org>
2015-11-12 11:23:40 -0800

    YARN-4347. Resource manager fails with Null pointer exception. (Jian He via wangda)
    (cherry picked from commit 7f55a18071ebe1c5591ff8a410b227c855c3f428)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/rmapp/RMAppImpl.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/rmapp/RMAppImpl.java
@@ -1290,7 +1290,7 @@ public class RMAppImpl implements RMApp, Recoverable {
         || appState == RMAppState.KILLED;
   }
   
-  private RMAppState getRecoveredFinalState() {
+  public RMAppState getRecoveredFinalState() {
     return this.recoveredFinalState;
   }
 
