ea5f81840dc79e9bd60d5ca747cee07a4f2bf6bc
Alexandr Balitsky <abalitsky@mapr.com>
2017-02-09 16:37:08 +0200

    MAPR-26008: YarnConfiguration.DEFAULT_NM_RECOVERY_ENABLED field has been changed to "true" and default NodeManager IPC port to 8099

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-app/src/test/java/org/apache/hadoop/mapreduce/jobhistory/TestJobHistoryEventHandler.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-app/src/test/java/org/apache/hadoop/mapreduce/jobhistory/TestJobHistoryEventHandler.java
@@ -573,6 +573,7 @@ public class TestJobHistoryEventHandler {
   public void testTimelineEventHandling() throws Exception {
     TestParams t = new TestParams(RunningAppContext.class, false);
     Configuration conf = new YarnConfiguration();
+    conf.setBoolean(YarnConfiguration.NM_RECOVERY_ENABLED, false);
     conf.setBoolean(YarnConfiguration.TIMELINE_SERVICE_ENABLED, true);
     long currentTime = System.currentTimeMillis();
     try (MiniYARNCluster yarnCluster = new MiniYARNCluster(
