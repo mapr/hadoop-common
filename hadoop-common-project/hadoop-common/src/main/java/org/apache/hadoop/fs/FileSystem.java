34e5be7c68975d1ac6daf3e4063274f2f5f7eb51
Alexandr Balitsky <abalitsky@mapr.com>
2017-07-12 13:47:41 +0300

    MAPR-27919. Memory Leak in org.apache.hadoop.fs.FileSystem$Cache. (#152)
    Added property:
    "fs.cache.lru.enable" - Enable/Disable LRU cache for the FileSystem entries (Default value: false).
    Enabled LRU cache only for RM by default (set fs.cache.lru.enable=true in yarn-env.sh)
    
    Also changed the names of properties for configuration LRU cache:
    1. "fs.cache.lru.entries.max-size" - The maximum number of entries the FileSystem cache may contain (Default value: 10000 entries).
    2. "fs.cache.lru.entries.expire-after-access-ms" - The length of time after an entry is last accessed
    that it should be automatically removed from the FileSystem cache (Default value: 60000 milliseconds).
    You can redefine these properties in your core-site.xml file, bases on performance indicators of your cluster.

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/FileSystem.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/FileSystem.java
@@ -46,7 +46,12 @@ import java.util.Stack;
 import java.util.TreeSet;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.apache.hadoop.thirdparty.com.google.common.cache.CacheBuilder;
+import org.apache.hadoop.thirdparty.com.google.common.cache.RemovalListener;
+import org.apache.hadoop.thirdparty.com.google.common.cache.RemovalNotification;
 
 import org.apache.hadoop.classification.InterfaceAudience;
 import org.apache.hadoop.classification.InterfaceStability;
@@ -3634,7 +3639,10 @@ public abstract class FileSystem extends Configured
   static final class Cache {
     private final ClientFinalizer clientFinalizer = new ClientFinalizer();
 
-    private final Map<Key, FileSystem> map = new HashMap<>();
+    Configuration conf = new Configuration();
+
+    private final Map<Key, FileSystem> map = getCacheMap(conf);
+
     private final Set<Key> toAutoClose = new HashSet<>();
 
     /** Semaphore used to serialize creation of new FS instances. */
@@ -3649,6 +3657,50 @@ public abstract class FileSystem extends Configured
 
     /** A variable that makes all objects in the cache unique. */
     private static AtomicLong unique = new AtomicLong(1);
+
+    private Map<Key, FileSystem> getCacheMap(Configuration conf) {
+      Map<Key, FileSystem> map;
+
+      boolean isFsLruEnable = conf.getBoolean(
+              CommonConfigurationKeys.FS_CACHE_LRU_ENABLE,
+              CommonConfigurationKeys.DEFAULT_FS_CACHE_LRU_ENABLE);
+
+      //Enable/Disable LRU cache only for current process
+      String property = System.getProperty(CommonConfigurationKeys.FS_CACHE_LRU_ENABLE);
+      isFsLruEnable = property == null ? isFsLruEnable : Boolean.parseBoolean(property);
+
+      if (isFsLruEnable) {
+        map = createLruCacheMap(conf);
+      } else {
+        map = new HashMap<>();
+      }
+
+      return map;
+    }
+
+    private Map<Key, FileSystem> createLruCacheMap(Configuration conf) {
+      final int FS_CACHE_LRU_ENTRIES_MAX_SIZE = conf.getInt(
+              CommonConfigurationKeys.FS_CACHE_LRU_ENTRIES_MAX_SIZE,
+              CommonConfigurationKeys.DEFAULT_FS_CACHE_LRU_ENTRIES_MAX_SIZE);
+
+      final int FS_CACHE_LRU_ENTRIES_EXPIRE_AFTER_ACCESS_MS = conf.getInt(
+              CommonConfigurationKeys.FS_CACHE_LRU_ENTRIES_EXPIRE_AFTER_ACCESS_MS,
+              CommonConfigurationKeys.DEFAULT_FS_CACHE_LRU_ENTRIES_EXPIRE_AFTER_ACCESS_MS);
+
+      Map<Key, FileSystem> map = CacheBuilder.newBuilder()
+              .maximumSize(FS_CACHE_LRU_ENTRIES_MAX_SIZE)
+              .expireAfterAccess(FS_CACHE_LRU_ENTRIES_EXPIRE_AFTER_ACCESS_MS, TimeUnit.MILLISECONDS)
+              .removalListener(new RemovalListener<Key, FileSystem>() {
+                @Override
+                public void onRemoval(RemovalNotification<Key, FileSystem> removal) {
+                  toAutoClose.remove(removal.getKey());
+                }
+              })
+              .build()
+              .asMap();
+
+      return map;
+    }
 
     /**
      * Instantiate. The configuration is used to read the
