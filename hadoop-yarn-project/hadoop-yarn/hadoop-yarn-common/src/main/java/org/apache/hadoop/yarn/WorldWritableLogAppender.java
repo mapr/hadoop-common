defd1ce853bfc6a0d6285c2290e2681d21583ae0
Oleksandr Shevchenko <oshevchenko@mapr.com>
2018-06-07 13:25:03 +0300

    Revert "MAPR-30945. yarn node -list command is failing with "java.lang.NoClassDefFoundError: Could not initialize class org.apache.log4j.LogManager""
    This reverts commit 1d392d0

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/WorldWritableLogAppender.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/WorldWritableLogAppender.java
@@ -1,14 +1,13 @@
 package org.apache.hadoop.yarn;
 
+import org.apache.hadoop.fs.FileContext;
 import org.apache.hadoop.fs.FileUtil;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.permission.FsPermission;
 import org.apache.log4j.RollingFileAppender;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.attribute.PosixFilePermission;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * WorldWritableLogAppender extends RollingFileAppender to create log file
@@ -19,24 +18,19 @@ public class WorldWritableLogAppender extends RollingFileAppender {
   @Override
   public synchronized void setFile(String fileName, boolean append,
                                    boolean bufferedIO, int bufferSize) throws IOException {
+    Path path  = new Path(fileName);
     File file = new File(fileName);
     File parent = file.getParentFile();
-    boolean isFirstCreate = !file.exists();
 
     if (FileUtil.canWrite(file) || FileUtil.canWrite(parent)) {
-      super.setFile(fileName, append, bufferedIO, bufferSize);
-    }
-    
-    if (isFirstCreate && file.exists()) {
-      Set<PosixFilePermission> permissions = new HashSet<>();
-      permissions.add(PosixFilePermission.OWNER_READ);
-      permissions.add(PosixFilePermission.OWNER_WRITE);
-      permissions.add(PosixFilePermission.GROUP_READ);
-      permissions.add(PosixFilePermission.GROUP_WRITE);
-      permissions.add(PosixFilePermission.OTHERS_READ);
-      permissions.add(PosixFilePermission.OTHERS_WRITE);
 
-      Files.setPosixFilePermissions(file.toPath(), permissions);
+      super.setFile(fileName, append, bufferedIO, bufferSize);
+
+      FileContext lfs = FileContext.getLocalFSFileContext();
+      FsPermission perm = FsPermission.createImmutable((short) 0666);
+      if (!lfs.getFileStatus(path).getPermission().equals(perm)) {
+        lfs.setPermission(path, perm);
+      }
     }
   }
 }
