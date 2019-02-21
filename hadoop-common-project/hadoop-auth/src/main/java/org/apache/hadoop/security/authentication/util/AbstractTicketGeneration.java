946d46eee214d87df2555da896210fa7bc47888d
Sergey Svynarchuk <ssvinarchuk@mapr.com>
2019-02-21 19:38:10 +0200

    Fixed methods signatures

--- hadoop-common-project/hadoop-auth/src/main/java/org/apache/hadoop/security/authentication/util/AbstractTicketGeneration.java
+++ hadoop-common-project/hadoop-auth/src/main/java/org/apache/hadoop/security/authentication/util/AbstractTicketGeneration.java
@@ -1,8 +1,10 @@
 package org.apache.hadoop.security.authentication.util;
 
+import java.io.IOException;
+
 public abstract class AbstractTicketGeneration {
 
-  public abstract void generateTicketAndSetServerKey();
+  public abstract void generateTicketAndSetServerKey() throws IOException;
 
-  public abstract void generateTicketAndSetServerKey(String clusterName);
+  public abstract void generateTicketAndSetServerKey(String clusterName) throws IOException;
 }
