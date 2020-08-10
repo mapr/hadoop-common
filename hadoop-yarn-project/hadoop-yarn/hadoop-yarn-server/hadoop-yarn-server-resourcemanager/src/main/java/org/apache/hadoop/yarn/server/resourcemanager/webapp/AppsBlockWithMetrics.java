apache/hadoop

HADOOP-11875. [JDK9] Adding a second copy of Hamlet without _ as a one-character identifier.

hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/webapp/AppsBlockWithMetrics.java
@@ -18,7 +18,6 @@
 
 package org.apache.hadoop.yarn.server.resourcemanager.webapp;
 
-import org.apache.hadoop.yarn.server.webapp.AppsBlock;
 import org.apache.hadoop.yarn.webapp.view.HtmlBlock;
 
 /**
@@ -26,7 +25,7 @@ import org.apache.hadoop.yarn.webapp.view.HtmlBlock;
  */
 class AppsBlockWithMetrics extends HtmlBlock {
   @Override public void render(Block html) {
-    html._(MetricsOverviewTable.class);
-    html._(RMAppsBlock.class);
+    html.__(MetricsOverviewTable.class);
+    html.__(RMAppsBlock.class);
   }
 }

mapr/hadoop-common

hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/webapp/AppsBlockWithMetrics.java
@@ -26,7 +26,7 @@ import org.apache.hadoop.yarn.webapp.view.HtmlBlock;
  */
 class AppsBlockWithMetrics extends HtmlBlock {
   @Override public void render(Block html) {
-    html._(MetricsOverviewTable.class);
-    html._(AppsBlock.class);
+    html.__(MetricsOverviewTable.class);
+    html.__(AppsBlock.class);
   }
 }