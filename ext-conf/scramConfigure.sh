336907aa52c1ccb2ebb82c42ac3755603d06cbb8
Sergey Svynarchuk <sergey.svinarchuk@hpe.com>
2022-01-17 14:00:52 +0200

    MAPRYARN-326: Added support for mix-nodes cluster

--- ext-conf/scramConfigure.sh
+++ ext-conf/scramConfigure.sh
@@ -53,19 +53,34 @@ function ConfigureScram() {
         export MAPR_TICKETFILE_LOCATION="${MAPR_HOME}/conf/mapruserticket"
     fi
     if [ "$isFips" != "true" ]; then
-      credType="jceks"
+        credType="jceks"
     else
-      credType="bcfks"
+        credType="bcfks"
     fi
     SCRAM_CREDS_FILE=$HADOOP_SCRAM_CONF_DIR/scram.${credType}
     SCRAM_CREDS_FILE_WITH_STORE=local${credType}://file$SCRAM_CREDS_FILE
+    CLEAR_PASS=$HADOOP_SCRAM_CONF_DIR/scram-password.txt
     if [ ! -f $SCRAM_CREDS_FILE ];then
         hadoop fs -test -f $YARN_PATH/scram.${credType}
         if [ $? -ne 0 ]; then
             if [ "$serviceName" == "resourceManager" ];then
                 local SCRAM_PASSW=`maprcli security genkey 2>/dev/null | head -c8`
                 hadoop credential create scram.password -value $SCRAM_PASSW -provider $SCRAM_CREDS_FILE_WITH_STORE &> /dev/null
+                echo "$SCRAM_PASSW" > $CLEAR_PASS
+                chmod 400 $CLEAR_PASS
                 hadoop fs -put -p $SCRAM_CREDS_FILE $YARN_PATH
+                hadoop fs -put -p $CLEAR_PASS $YARN_PATH
+                rm -f $CLEAR_PASS
+            else
+                hadoop fs -test -f $YARN_PATH/scram-password.txt
+                if [ $? -eq 0 ]; then
+                    local SCRAM_PASSW=`hadoop fs -cat $YARN_PATH/scram-password.txt`
+                    hadoop credential create scram.password -value $SCRAM_PASSW -provider $SCRAM_CREDS_FILE_WITH_STORE &> /dev/null
+                    hadoop fs -put -p $SCRAM_CREDS_FILE $YARN_PATH
+                else
+                    echo "Please check that RM is started and generated SCRAM password."
+                    exit 1
+                fi
             fi
         else
           hadoop fs -copyToLocal -p $YARN_PATH/scram.${credType} $SCRAM_CREDS_FILE
