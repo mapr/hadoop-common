a01858882ccfd18bf5952e52f3adc1640c5340a6
Sergey Svinarchuk <sergey.svinarchuk@hpe.com>
2022-08-26 10:04:29 +0300

    MAPRHADOOP-287: Fixed DistributedFSCheck for KV file types

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient/src/test/java/org/apache/hadoop/fs/DistributedFSCheck.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient/src/test/java/org/apache/hadoop/fs/DistributedFSCheck.java
@@ -137,6 +137,10 @@ public class DistributedFSCheck {
       listSubtree(symlink, writer);
       return;
     }
+    //KV file types is not a file or directory. This type returns itself during listing, that's why should skip listing for them.
+    if (!rootStatus.isDirectory() && !rootStatus.isFile() && !rootStatus.isSymlink()) {
+      return;
+    }
     
     FileStatus [] children = null;
     try {
