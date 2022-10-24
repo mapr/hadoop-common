acf7af4dcd15f0093702ceaadf2da95d2ca0ba9c
Serhii Severin <sergei.severin@hpe.com>
2022-10-24 14:49:13 +0300

    MAPRHADOOP-324: Distcp creates a directory for every file

--- hadoop-tools/hadoop-distcp/src/main/java/org/apache/hadoop/tools/SimpleCopyListing.java
+++ hadoop-tools/hadoop-distcp/src/main/java/org/apache/hadoop/tools/SimpleCopyListing.java
@@ -505,11 +505,7 @@ public class SimpleCopyListing extends CopyListing {
 
 
     if (solitaryFile) {
-      if (!targetPathExists || targetFS.isFile(target)) {
-        return sourceStatus.getPath();
-      } else {
-        return sourceStatus.getPath().getParent();
-      }
+      return sourceStatus.getPath();
     } else {
       boolean specialHandling =
           (context.getSourcePaths().size() == 1 &&
@@ -517,9 +513,11 @@ public class SimpleCopyListing extends CopyListing {
               context.shouldSyncFolder() ||
               context.shouldOverwrite();
 
-      if ((specialHandling && (sourceStatus.isDirectory() ||
-              (sourceStatus.isSymlink() && getOriginalFileStatus(sourceStatus, getConf(), context.shouldKeepLinks(), loopLocator).getSourceRealPath().isDirectory()))) ||
-          sourceStatus.getPath().isRoot()) {
+      boolean simpleFile = !sourceStatus.isDirectory() &&
+              !(sourceStatus.isSymlink() && getOriginalFileStatus(sourceStatus, getConf(), context.shouldKeepLinks(), loopLocator).getSourceRealPath().isDirectory());
+      if ((specialHandling && (sourceStatus.isDirectory() || !simpleFile)) ||
+              simpleFile ||
+              sourceStatus.getPath().isRoot()) {
         return sourceStatus.getPath();
       } else {
         return sourceStatus.getPath().getParent();
