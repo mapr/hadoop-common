88c9d528d588525038821d8c5be68393008bbc55
Sergey Svinarchuk <sergey.svinarchuk@hpe.com>
2023-08-10 10:22:16 +0300

    MAPRHADOOP-399: TestTimelineAuthFilterForV2 add hadoop.login kerberos for ATSv2 tests

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-tests/src/test/java/org/apache/hadoop/yarn/server/timelineservice/security/TestTimelineAuthFilterForV2.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-tests/src/test/java/org/apache/hadoop/yarn/server/timelineservice/security/TestTimelineAuthFilterForV2.java
@@ -155,6 +155,7 @@ public class TestTimelineAuthFilterForV2 {
       conf = new Configuration(false);
       conf.setClass("fs.file.impl", RawLocalFileSystem.class,
           FileSystem.class);
+      System.setProperty("hadoop.login", "kerberos");
       conf.setStrings(TIMELINE_HTTP_AUTH_PREFIX + "type",
           "kerberos");
       conf.set(CommonConfigurationKeys.CUSTOM_AUTH_METHOD_PRINCIPAL_CLASS_KEY,
