e13c30b174204a1d399ff797a9fd10f5e34e0d12
Sergey Svinarchuk <sergey.svinarchuk@hpe.com>
2024-09-05 14:49:57 +0300

    MAPRHADOOP-404: Fixed TestHarFileSystem tests. Added not implemented methods

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/TestHarFileSystem.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/TestHarFileSystem.java
@@ -36,6 +36,7 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.net.InetSocketAddress;
 import java.util.Collection;
 import java.util.EnumSet;
 import java.util.Iterator;
@@ -259,6 +260,35 @@ public class TestHarFileSystem {
     Path getEnclosingRoot(Path path) throws IOException;
 
     BulkDelete createBulkDelete(Path path) throws IllegalArgumentException, IOException;
+
+    public PathId createPathId();
+
+    public FSDataInputStream openFid2(PathId pfid, String file,  int readAheadBytesHint)
+            throws IOException;
+
+    public FSDataOutputStream createFid(String pfid, String file)
+            throws IOException;
+
+    public boolean deleteFid(String pfid, String dir)
+            throws IOException;
+
+    public String mkdirsFid(Path p) throws IOException;
+
+    public String mkdirsFid(String pfid, String dir)
+            throws IOException;
+
+    public void setOwnerFid(String fid, String user, String group) throws IOException;
+
+    public FSDataInputStream openFid(String fid, long[] ips,
+                                     long chunkSize, long fileSize) throws IOException;
+
+    public FSDataInputStream openFid(String pfid, String file, long [] ips)
+            throws IOException;
+
+    public String getZkConnectString() throws IOException;
+
+    public InetSocketAddress[] getJobTrackerAddrs(Configuration conf)
+            throws IOException;
   }
 
   @Test
