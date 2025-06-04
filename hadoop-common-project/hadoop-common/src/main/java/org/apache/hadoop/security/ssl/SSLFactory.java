5ec60ed0e13308ce5986e2786596b672cd36e845
Sergey Svinarchuk <sergey.svinarchuk@hpe.com>
2025-06-04 14:40:37 +0300

    Revert HADOOP-15760 - additional import fix

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/ssl/SSLFactory.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/security/ssl/SSLFactory.java
@@ -17,7 +17,7 @@
 */
 package org.apache.hadoop.security.ssl;
 
-import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.hadoop.classification.InterfaceAudience;
 import org.apache.hadoop.classification.InterfaceStability;
 import org.apache.hadoop.conf.Configuration;
